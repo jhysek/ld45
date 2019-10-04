bc13c246b8c5baf3650bf7ca2b83c6ad7dc43d6f
Jiri Hysek <jiri.hysek@gmail.com>
2019-10-04 16:28:50 +0200

    Empty game project

---
+++ Scenes/Menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _on_StartBtn_pressed():
+	$Sfx/Click.play()
+	get_tree().change_scene("res://Scenes/Game.tscn")
+
+func _on_StartBtn_mouse_entered():
+	$Sfx/Hover.play()
+
+func _on_StartBtn_mouse_exited():
+	$Sfx/Hover.play()
